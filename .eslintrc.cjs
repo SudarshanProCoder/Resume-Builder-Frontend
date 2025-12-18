e54a96f96d58240c4c3fbb134651f7af01adca50
SudarshanProCoder <sudarshandate21@gmail.com>
2025-12-19 02:03:22 +0530

    CI pipeline integrated

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,19 +1,33 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
+  env: {
+    browser: true,
+    es2021: true,
+  },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:prettier/recommended",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "prettier"],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
+    "no-warning-comments": [
+      "error",
+      { terms: ["todo", "fixme"], location: "anywhere" },
     ],
-    '@typescript-eslint/no-explicit-any': 'warn',
+    "spaced-comment": ["error", "never"],
+
+    "no-inline-comments": "error",
+    "no-multi-str": "off",
+
+    "prettier/prettier": "error",
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
   },
 };
